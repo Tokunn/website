fc1356d9fafa78fe4fa12fdda9efe06c84451eca
Tokunn <you@example.com>
2015-06-29 19:53:21 +0900

    update

--- root/common/template/template.php
+++ root/common/template/template.php
@@ -21,13 +21,27 @@ $path_list = [
     'robot'     => '/bookshelf/robot/',
 ];
 
+# Analytics
+$analyticstracking = <<< EOF
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+              m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-64549299-1', 'auto');
+    ga('send', 'pageview');
+
+</script>
+EOF;
+
 # Head
 $head = <<< EOF
 <!DOCTYPE html>
 <html lang="ja">
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>絵のない技術書</title>
+        <title>絵のない技術書 - %s</title>
         <link rel="stylesheet" href="/common/css/normalize.css">
         <link rel="stylesheet" href="/common/css/style.css">
         <link rel="stylesheet" href="/common/syntaxhighlighter/styles/shCoreRDark.css">
@@ -42,6 +56,7 @@ $head = <<< EOF
     </head>
 
     <body>
+        %s
         <div id="page">
 
             <header id="pageHead">
@@ -79,10 +94,28 @@ $foot = <<< EOF
 </html>
 EOF;
 
+# Check Analytics()
+function check_analytics() {
+    global $analyticstracking;
+    $dirname_new = __FILE__;
+    $dirname;
+    while($dirname_new != '/') {
+        $dirname = $dirname_new;
+        $dirname_new = dirname($dirname_new);
+    }
+
+    if ($dirname == '/srv') {
+        $analytics_code = '<!-- Test System -->';
+    } else {
+        $analytics_code = $analyticstracking;
+    }
+    return $analytics_code;
+}
+
 # Head()
-function set_head_path($path) {
+function set_head_path($title, $path) {
     global $head;
-    echo(sprintf($head, $path));
+    echo(sprintf($head, $title, check_analytics(), $path));
 }
 
 # Foot()
@@ -91,8 +124,9 @@ function set_foot() {
     echo($foot);
 }
 
+
 # Head and Path
-function set_head($dir) {
+function set_head($title, $dir) {
     global $path_name;
     global $path_list;
     $path_template = " &gt; <a href=\"%s\">%s</a>";
@@ -105,7 +139,7 @@ function set_head($dir) {
         $dir = dirname($dir);
         $path = sprintf($path_template, $path_list[$base_dir], $path_name[$base_dir]).$path;
     }
-    set_head_path($path);
+    set_head_path($title, $path);
 }
 
 #include_once("analyticstracking.php");
